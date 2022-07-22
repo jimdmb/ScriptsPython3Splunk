ea923a1bb358edfbf490a04ba3cf21f5ba043179
jimdmb <34405334+jimdmb@users.noreply.github.com>
2022-07-21 21:10:26 -0400

    update script with python3 compatibility
    I removed the block that was adding search because my search use  '| loadjob savedsearch="nobody:search:<report_name_to_change>"'

--- splunksearch.py
+++ splunksearch.py
@@ -14,45 +14,41 @@ import json
 baseurl = '[redacted]'
 username = '[redacted]'
 password = '[redacted]'
-
+searchquery = 'search query'
 myhttp = httplib2.Http(disable_ssl_certificate_validation=True)
 
 #Step 1: Get a session key
 servercontent = myhttp.request(baseurl + '/services/auth/login', 'POST',
-headers={}, body=urllib.urlencode({'username':username, 'password':password}))[1]
+headers={}, body=urllib.parse.urlencode({'username':username, 'password':password}))[1]
 sessionkey = minidom.parseString(servercontent).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
-#print "====>sessionkey:  %s  <====" % sessionkey
+#print ("====>sessionkey:  %s  <====" % sessionkey)
 
 #Step 2: Create a search job. remove wifi, vpn, perimeter
-searchquery = 'query'
-if not searchquery.startswith('search'):
-	searchquery = 'search ' + searchquery
-
 searchjob = myhttp.request(baseurl + '/services/search/jobs','POST',
-headers={'Authorization': 'Splunk %s' % sessionkey},body=urllib.urlencode({'search': searchquery}))[1]
+headers={'Authorization': 'Splunk %s' % sessionkey},body=urllib.parse.urlencode({'search': searchquery}))[1]
 sid = minidom.parseString(searchjob).getElementsByTagName('sid')[0].childNodes[0].nodeValue
-#print "====>sid:  %s  <====" % sid
+#print ("====>sid:  %s  <====" % sid)
 
 #Step 3: Get the search status
 myhttp.add_credentials(username, password)
 servicessearchstatusstr = '/services/search/jobs/%s/' % sid
 isnotdone = True
 while isnotdone:
-    searchstatus = myhttp.request(baseurl + servicessearchstatusstr, 'GET')[1]
-    isdonestatus = re.compile('isDone">(0|1)')
-    isdonestatus = isdonestatus.search(searchstatus).groups()[0]
-    if (isdonestatus == '1'):
-        isnotdone = False
-#print "====>search status:  %s  <====" % isdonestatus
+   searchstatus = myhttp.request(baseurl + servicessearchstatusstr, 'GET')[1]
+   searchstatus = str(searchstatus)
+   if searchstatus.find('isDone">1') !=-1:
+       isnotdone = False
+#print ("====>search status:  %s  <====" % isdonestatus)
 
 #Step 4: Get the search results
 services_search_results_str = '/services/search/jobs/%s/results?output_mode=json&count=0' % sid
 #services_search_results_str = '/services/search/jobs/%s/results' % sid
 searchresults = myhttp.request(baseurl + services_search_results_str, 'GET')[1]
-#print "====>search result:  [%s]  <====" % searchresults
+#print ("====>search result:  [%s]  <====" % searchresults)
 
 ############junk
 y = json.loads(searchresults)
+print(y)
 for results in y['results']:
-        for key,value in results.iteritems():
-                print value
+        for key,value in results.items():
+                print (value)
